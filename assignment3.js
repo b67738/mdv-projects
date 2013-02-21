d354cddc6f0dc7c297f920f991dee8520e4c7d11
Randall Fulmer <whiful@fullsail.edu>
2013-02-20 22:02:04 -0600

    dazed and confused

--- assignment3.js
+++ assignment3.js
@@ -16,25 +16,45 @@ var hero = {
 	
 };
 
+var monster = {
+	"name": "Gannon",
+	"HP": 200
+};
+
 //global variables
 var sword = "Master Sword"
 var armor = "Royal Armor"
 var horse = "Pebbles"
+var monsters = ["wolves", "bears", "skeletons", "gnolls"]
+var night = true
 
 //method procedure
 var battleland = {
-	zone: "badlands",
-	mobs: true,
+	zone: "badlands", //property string
+	mobs: true, //property boolean
 	fight: function() {
 		if (this.mobs === true) {
-		console.log("My name is " + hero.name + " the " + hero.race + " " + hero.prof + ". Looks like there are monsters that need slaying!")
+		console.log("My name is " + hero.name + " the " + hero.race + " " + hero.prof + ". As always, there are monsters that need slaying!")
+			if (this.zone === "badlands") { //nested conditional
+			console.log("The badlands are a dangerous place!")
+			}
 		} else {
 		console.log("Nothing to see here, keep moving.")
 		}
 	}
 };
 
+//boolean function
+var difficulty = function (nightTime){
+	if (nightTime === true) {
+		monster.HP = monster.HP + 50
+	}
+	return difficulty
+};
+
 //
+
+
 
 
 
