597d775bd8f88b3f700de12da3e7864c69714b64
Randall Fulmer <whiful@fullsail.edu>
2013-02-20 19:58:34 -0600

    Updated Code

--- assignment3.js
+++ assignment3.js
@@ -1,12 +1,41 @@
 //alert("JavaScript works!");
 
+// Randall Fulmer
+// SDI 1302
+// Project 3
+// Korx's Journey
+
 //JSON Data
-var Korx = {
+var hero = {
 	"name": "Korx",
-	"class": "Paladin",
-	"level": 50
-	"HP": 5000
+	"race": "Dwarf",
+	"prof": "Paladin",
+	"level": 50,
+	"HP": 5000,
 	"MP": 2000
 	
 };
+
+//global variables
+var sword = "Master Sword"
+var armor = "Royal Armor"
+var horse = "Pebbles"
+
+//method procedure
+var battleland = {
+	zone: "badlands",
+	mobs: true,
+	fight: function() {
+		if (this.mobs === true) {
+		console.log("My name is " + hero.name + " the " + hero.race + " " + hero.prof + ". Looks like there are monsters that need slaying!")
+		} else {
+		console.log("Nothing to see here, keep moving.")
+		}
+	}
+};
+
+//
+
+
+
 
